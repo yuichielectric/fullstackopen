d6c54a1d4609971836ce2b342b123816010327fe
Yuichi Tanaka <yuichielectric@github.com>
2019-12-15 15:49:40 +0900

    Undo auto formatting

--- part1/src/App.js
+++ part1/src/App.js
@@ -3,32 +3,23 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const now = new Date();
-  return ( <
-    div className = "App" >
-    <
-    header className = "App-header" >
-    <
-    img src = {
-      logo
-    }
-    className = "App-logo"
-    alt = "logo" / >
-    <
-    p >
-    Edit < code > src / App.js < /code> and save to reload. <
-    /p> <
-    p > It is {
-      now.toString()
-    } < /p> <
-    a className = "App-link"
-    href = "https://reactjs.org"
-    target = "_blank"
-    rel = "noopener noreferrer" >
-    Learn React <
-    /a> <
-    /header> <
-    /div>
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
